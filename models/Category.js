3afd3c5b35466b3b36539ce66f693d675f56982c
Jake Swackhammer <j.100101@gmail.com>
2021-10-26 18:21:34 -0400

    updated category model

--- models/Category.js
+++ models/Category.js
@@ -7,11 +7,14 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    category_name:{
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
     timestamps: false,
-    freezeTableName: true,
     underscored: true,
     modelName: 'category',
   }
