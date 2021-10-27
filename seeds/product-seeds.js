b57de12ca2f14e2b29f253a5b3d7f4db27ae790e
Jake Swackhammer <j.100101@gmail.com>
2021-10-26 20:56:20 -0400

    dropped underscore naming convention for clarity

--- seeds/product-seeds.js
+++ seeds/product-seeds.js
@@ -2,31 +2,31 @@ const { Product } = require('../models');
 
 const productData = [
   {
-    product_name: 'Plain T-Shirt',
+    productName: 'Plain T-Shirt',
     price: 14.99,
     stock: 14,
     categoryId: 1,
   },
   {
-    product_name: 'Running Sneakers',
+    productName: 'Running Sneakers',
     price: 90.0,
     stock: 25,
     categoryId: 5,
   },
   {
-    product_name: 'Branded Baseball Hat',
+    productName: 'Branded Baseball Hat',
     price: 22.99,
     stock: 12,
     categoryId: 4,
   },
   {
-    product_name: 'Top 40 Music Compilation Vinyl Record',
+    productName: 'Top 40 Music Compilation Vinyl Record',
     price: 12.99,
     stock: 50,
     categoryId: 3,
   },
   {
-    product_name: 'Cargo Shorts',
+    productName: 'Cargo Shorts',
     price: 29.99,
     stock: 22,
     categoryId: 2,
